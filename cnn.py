b46affc8fcf62eda446cbb417266d29ba63ba62f
gabz81y <g.czapska@umail.leidenuniv.nl>
2025-05-08 21:10:32 +0200

    transform_update

--- cnn.py
+++ cnn.py
@@ -28,11 +28,10 @@ if torch.cuda.is_available():
 
 # Define separate transforms for training and testing
 train_transform = transforms.Compose([
-    transforms.Resize(144),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
+    transforms.RandomRotation(20),
+    transforms.RandomResizedCrop(128, scale=(0.8,1.0)),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-    transforms.CenterCrop(128),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
